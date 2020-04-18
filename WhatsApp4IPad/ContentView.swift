10bb3e9fc7b018c4f298dcdc2202e0d2e9cd5d04
Can Çitoğlu <cancitoglu@gmail.com>
2020-04-18 21:02:04 +0300

    Initial Commit

---
+++ WhatsApp4IPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsApp4IPad
+//
+//  Created by Can Citoglu on 18.04.2020.
+//  Copyright © 2020 Can Citoglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
